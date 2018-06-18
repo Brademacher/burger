e893e79794a658688d10a86bd70bd30dd035dbbf
unknown <benjamin.rademacher@gmail.com>
2018-06-18 17:00:15 -0500

    start of assignment

---
+++ config/orm.js
@@ -0,0 +1,85 @@
+// MySQL connection
+var connect = require("../config/connection.js");
+
+// Function to create string of "?" for mySQL
+function questionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Function to convert object key/value pairs to SQL syntax
+function sqlOb(ob) {
+    var arr = [];
+
+    //looping through keys and pushing the key/value as a string in arr
+    for (var key in ob) {
+        var value = ob[key];
+        //check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            //if string with spaces, add quotations
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
+
+// orm object for the SQL statement function
+var orm = {
+    selectAll: function (tableInput, cd) {
+        var querystring = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
+    },
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+}
+
+// Export the orm object
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('New Bacon-ings', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rest in Peas', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fig-eta Bout it', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pablano Picaso', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Olive and Let Die', false);
